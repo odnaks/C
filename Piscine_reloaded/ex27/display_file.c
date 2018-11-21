5201d5a259811ed8dbe9eedcd1950836c50a330a
Dremora Restless draugr <drestles@e4r11p12.21-school.ru>
2018-11-21 17:23:54 +0300

    edit ex27

--- Piscine_reloaded/ex27/display_file.c
+++ Piscine_reloaded/ex27/display_file.c
@@ -6,7 +6,7 @@
 /*   By: drestles <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/20 21:19:43 by drestles          #+#    #+#             */
-/*   Updated: 2018/11/20 21:52:41 by drestles         ###   ########.fr       */
+/*   Updated: 2018/11/21 16:01:46 by drestles         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@ void	text_out(char *filename)
 	char	c;
 
 	file = open(filename, O_RDONLY);
+	if (file < 0)
+		return ;
 	while (read(file, &c, 1))
 		write(1, &c, 1);
 	close(file);
@@ -27,7 +29,7 @@ void	text_out(char *filename)
 int		main(int argc, char **argv)
 {
 	if (argc == 1)
-		write(2, "File name missing.\n", 20);
+		write(2, "File name missing.\n", 19);
 	else if (argc > 2)
 		write(2, "Too many arguments.\n", 20);
 	else
