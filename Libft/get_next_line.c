8dbe57fc9e2b2d21a73234990bafadc215af765e
odnaks <43475234+odnaks@users.noreply.github.com>
2018-11-28 01:43:34 +0300

    without structs

--- Libft/get_next_line.c
+++ Libft/get_next_line.c
@@ -6,19 +6,19 @@
 /*   By: drestles <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/26 20:45:32 by drestles          #+#    #+#             */
-/*   Updated: 2018/11/26 21:53:39 by drestles         ###   ########.fr       */
+/*   Updated: 2018/11/28 00:24:07 by drestles         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include "./libft/libft.h"
+#include "./libft/includes/libft.h"
 
-static char    *ft_strndup(const char *s1, size_t n)
+static char		*ft_strndup(const char *s1, size_t n)
 {
-	char        *str_copy;
-	size_t        i;
-	
-	str_copy = malloc(sizeof(*str_copy) * (n + 1));
+	char	*str_copy;
+	size_t	i;
+
+	str_copy = (char *)malloc(n + 1);
 	if (!str_copy)
 		return (NULL);
 	i = 0;
@@ -45,8 +45,8 @@ static void		add_new_chars(char **str, char *buffer, int size)
 {
 	char *temp;
 
-    if (!(*str))
-        *str = (char*)malloc(sizeof(char));
+	if (!(*str))
+		*str = ft_strnew(1);
 	if (*str)
 	{
 		buffer[size] = '\0';
@@ -56,31 +56,28 @@ static void		add_new_chars(char **str, char *buffer, int size)
 	}
 }
 
-int		get_next_line(const int fd, char **line)
+int				get_next_line(const int fd, char **line)
 {
-	static	t_lines lines[MAX_FD];
-	char	buffer[BUFF_SIZE];
-	int		len;
-	int		size;
+	static char	*lines[MAX_FD];
+	char			buffer[BUFF_SIZE+1];
+	int				len;
+	int				size;
 
 	if (fd < 0 || line == NULL || fd > MAX_FD || read(fd, buffer, 0) == -1)
 		return (-1);
-	while ((size = read(fd, buffer, BUFF_SIZE)))
-	{
-		add_new_chars(&lines[fd].read, buffer, size);
-		lines[fd].send = lines[fd].read;
-	}
+	while ((size = read(fd, buffer, BUFF_SIZE)) > 0)
+		add_new_chars(&lines[fd], buffer, size);
 	if (size == -1)
 		return (-1);
-	len = str_len(lines[fd].send);
-	if (!(*line = ft_strndup(lines[fd].send, len)))
+	len = str_len(lines[fd]);
+    if (*(lines[fd]) == '\0')
+    {
+        free(*line);
+        return (0);
+    }
+    if (!(*line = ft_strndup(lines[fd], len)))
 		return (-1);
-	if (*(lines[fd].send) == '\0')
-	{
-		free(lines[fd].read);
-		return (0);
-	}
 	len++;
-	lines[fd].send += len;
+	lines[fd] += len;
 	return (1);
 }
--- Libft/get_next_line.h
+++ Libft/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: drestles <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/26 19:40:44 by drestles          #+#    #+#             */
-/*   Updated: 2018/11/26 19:54:01 by drestles         ###   ########.fr       */
+/*   Updated: 2018/11/28 00:06:10 by drestles         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,9 @@
 # define GET_NEXT_LINE_H
 # include <stdlib.h>
 # include <string.h>
-
 # define BUFF_SIZE 32
-# define MAX_FD 1000
+# define MAX_FD 259
 
-int            get_next_line(const int fd, char **line);
-
-typedef	struct	s_lines
-{
-	char	*read;
-	char	*send;
-}				t_lines;
+int				get_next_line(const int fd, char **line);
 
 #endif
-
