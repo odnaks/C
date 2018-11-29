38f1e6e83dd5dba8663cf45ea6671d7543576daa
odnaks <43475234+odnaks@users.noreply.github.com>
2018-11-29 12:53:32 +0300

    new version

--- Libft/get_next_line.c
+++ Libft/get_next_line.c
@@ -3,81 +3,58 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: drestles <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: drestles <drestles@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/11/26 20:45:32 by drestles          #+#    #+#             */
-/*   Updated: 2018/11/28 00:24:07 by drestles         ###   ########.fr       */
+/*   Created: 2018/11/29 03:15:50 by drestles          #+#    #+#             */
+/*   Updated: 2018/11/29 12:47:32 by drestles         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
 #include "./libft/includes/libft.h"
+#include "get_next_line.h"
 
-static char		*ft_strndup(const char *s1, size_t n)
+static int	out_lines(char **line, char *reader)
 {
-	char	*str_copy;
-	size_t	i;
+	char *s;
 
-	str_copy = (char *)malloc(n + 1);
-	if (!str_copy)
-		return (NULL);
-	i = 0;
-	while (s1[i] != '\0' && i < n)
+	s = ft_strchr(reader, '\n');
+	if (s)
 	{
-		str_copy[i] = s1[i];
-		i++;
+		s[0] = '\0';
+		*line = ft_strdup(reader);
+		ft_memmove(reader, &s[1], ft_strlen(&s[1]) + 1);
+		return (1);
 	}
-	str_copy[i] = '\0';
-	return (str_copy);
-}
-
-static int		str_len(char *lines)
-{
-	int	i;
-
-	i = 0;
-	while (lines[i] != '\n' && lines[i] != '\0')
-		i++;
-	return (i);
-}
-
-static void		add_new_chars(char **str, char *buffer, int size)
-{
-	char *temp;
-
-	if (!(*str))
-		*str = ft_strnew(1);
-	if (*str)
+	else if (ft_strlen(reader) > 0)
 	{
+		*line = ft_strdup(reader);
+		*reader = 0;
+		return (1);
+	}
+	return (0);
+}
+
+int			get_next_line(const int fd, char **line)
+{
+	static char	*reader[MAX_FD];
+	char		buffer[BUFF_SIZE + 1];
+	int			size;
+	char		*temp;
+
+	if (fd < 0 || fd > MAX_FD || !line)
+		return (-1);
+	if (!reader[fd] && !(reader[fd] = ft_strnew(1)))
+		return (-1);
+	while (!ft_strchr(reader[fd], '\n'))
+	{
+		if ((size = read(fd, buffer, BUFF_SIZE)) == -1)
+			return (-1);
+		if (size == 0)
+			break ;
 		buffer[size] = '\0';
-		temp = *str;
-		*str = ft_strjoin(*str, buffer);
-		free(temp);
+		temp = ft_strjoin(reader[fd], buffer);
+		free(reader[fd]);
+		reader[fd] = temp;
 	}
-}
-
-int				get_next_line(const int fd, char **line)
-{
-	static char	*lines[MAX_FD];
-	char			buffer[BUFF_SIZE+1];
-	int				len;
-	int				size;
-
-	if (fd < 0 || line == NULL || fd > MAX_FD || read(fd, buffer, 0) == -1)
-		return (-1);
-	while ((size = read(fd, buffer, BUFF_SIZE)) > 0)
-		add_new_chars(&lines[fd], buffer, size);
-	if (size == -1)
-		return (-1);
-	len = str_len(lines[fd]);
-    if (*(lines[fd]) == '\0')
-    {
-        free(*line);
-        return (0);
-    }
-    if (!(*line = ft_strndup(lines[fd], len)))
-		return (-1);
-	len++;
-	lines[fd] += len;
-	return (1);
+	return (out_lines(line, reader[fd]));
 }
--- Libft/get_next_line.h
+++ Libft/get_next_line.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: drestles <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: drestles <drestles@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/26 19:40:44 by drestles          #+#    #+#             */
-/*   Updated: 2018/11/28 00:06:10 by drestles         ###   ########.fr       */
+/*   Updated: 2018/11/29 12:49:02 by drestles         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 # include <stdlib.h>
 # include <string.h>
 # define BUFF_SIZE 32
-# define MAX_FD 259
+# define MAX_FD 10240
 
 int				get_next_line(const int fd, char **line);
 
